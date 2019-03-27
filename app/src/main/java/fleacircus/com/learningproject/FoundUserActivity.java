edd0c8c1acb8398162239d00d2d2ad6266726e19
Sean Moloney <seanmolo1@gmail.com>
2019-03-27 17:13:28 +0000

    Updates to presentation of discovered users

--- app/src/main/java/fleacircus/com/learningproject/FoundUserActivity.java
+++ app/src/main/java/fleacircus/com/learningproject/FoundUserActivity.java
@@ -4,16 +4,34 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import fleacircus.com.learningproject.Classes.CustomUser;
 import fleacircus.com.learningproject.Helpers.GridImageAdapterHelper;
 import fleacircus.com.learningproject.Helpers.MenuHelper;
+import fleacircus.com.learningproject.Utils.StringUtils;
 
 public class FoundUserActivity extends AppCompatActivity {
 
     private void setSelectedUser() {
         CustomUser customUser = (CustomUser) getIntent().getSerializableExtra("user");
+
+        String n = StringUtils.toUpperCase(customUser.getName());
+        String l = StringUtils.toUpperCase(customUser.getLocation());
+        TextView name = findViewById(R.id.name);
+        TextView location = findViewById(R.id.location);
+        name.setText(n);
+        location.setText(l);
+
+        String t = customUser.getTeacherStudent();
+        if (StringUtils.hasMatch(t, getString(R.string.answer_teacher))) {
+            String c = customUser.getCollegeSchool();
+            if (StringUtils.hasMatch(c, getString(R.string.answer_college))) {
+                TextView course = findViewById(R.id.course);
+                course.setText(StringUtils.toUpperCase(customUser.getCourse()));
+            }
+        }
 
         int imageID = customUser.getImageID();
         if (imageID != 0) {
