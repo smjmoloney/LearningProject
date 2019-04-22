6c6ea6be3eaf14d01fd6d1157b2091ed21a33ec0
Sean <seanmolo1@gmail.com>
2019-04-22 16:23:16 +0100

    Large change to found user activity file supporting additional course presentation, new xml layout changes and/or rearrangement of methods and content

--- app/src/main/java/fleacircus/com/learningproject/FoundUserActivity.java
+++ app/src/main/java/fleacircus/com/learningproject/FoundUserActivity.java
@@ -1,153 +1,199 @@
 package fleacircus.com.learningproject;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import fleacircus.com.learningproject.Adapters.CourseAdapter;
-import fleacircus.com.learningproject.Classes.CustomCourse;
+import fleacircus.com.learningproject.Adapters.SectionsPagerAdapter;
 import fleacircus.com.learningproject.Classes.CustomUser;
+import fleacircus.com.learningproject.Classes.CustomViewPagerMeasure;
+import fleacircus.com.learningproject.Helpers.AnimationHelper;
 import fleacircus.com.learningproject.Helpers.GridImageAdapterHelper;
 import fleacircus.com.learningproject.Helpers.MenuHelper;
 import fleacircus.com.learningproject.Helpers.RecyclerHelper;
-import fleacircus.com.learningproject.Listeners.OnGetDataListener;
-import fleacircus.com.learningproject.Utils.CustomDatabaseUtils;
+import fleacircus.com.learningproject.Recyclers.CoursesFlashCardRecyclerFragment;
+import fleacircus.com.learningproject.Recyclers.CoursesQuizRecyclerFragment;
+import fleacircus.com.learningproject.Utils.CustomAnimationUtils;
+import fleacircus.com.learningproject.Utils.NavigationUtils;
 import fleacircus.com.learningproject.Utils.StringUtils;
 
 public class FoundUserActivity extends AppCompatActivity {
 
-    String foundUid;
+    @BindView(R.id.itemAlpha) View itemAlpha;
+    @BindView(R.id.linearLayoutSelector) LinearLayout linearLayoutSelector;
+    @BindView(R.id.viewPagerCourses) CustomViewPagerMeasure viewPagerCourses;
+    @BindView(R.id.textViewSelectorOne) TextView textViewSelectorOne;
+    @BindView(R.id.textViewSelectorTwo) TextView textViewSelectorTwo;
+    @BindView(R.id.textViewSend) TextView textViewSend;
 
-    @OnClick(R.id.buttonSubmit)
-    void sendDocument() {
-        if (foundUid.isEmpty())
-            return;
+    View.OnClickListener onClickListenerHome;
+    View.OnClickListener onClickListenerRecycler;
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
-        if (user == null)
-            return;
+    private String foundUid;
+    private boolean isFlashCard;
 
-        String uid = user.getUid();
-        String[] from =  new String[]{"users", foundUid, "courses", "poayKZr0qQwwOWudgrTz"};
-        String[] to =  new String[]{"users", uid, "courses"};
+    float alpha = 0;
+    long duration = 0;
 
-        CustomDatabaseUtils.copyDocument(from, to, "Classes.CustomCourse");
-        Toast.makeText(this, R.string.courses_message_addition, Toast.LENGTH_SHORT).show();
+    private Bundle applyCourseBundle(boolean isCreate) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean("isFlashCard", isFlashCard);
+        bundle.putBoolean("isCreate", isCreate);
+        bundle.putString("foundUid", foundUid);
+
+        return bundle;
     }
 
-    /**
-     * Method accesses the courses collection of the currently
-     * logged in user and populates our recycler view with each
-     * course. They are defined as documents, each with a number
-     * of fields.
-     */
-    private void setCourses() {
-        //noinspection ConstantConditions
-        String[] collection = new String[]{"users", foundUid, "courses"};
+    @OnClick(R.id.textViewSelectorOne)
+    void textViewSelectorOne() {
+        if (textViewSelectorOne.getText() != null)
+            isFlashCard = true;
 
-        CustomDatabaseUtils.read(collection, new OnGetDataListener() {
-            @Override
-            public void onStart() {
-
-            }
-
-            @Override
-            public void onSuccess(Object object, boolean isQuery) {
-                try {
-                    if (isQuery) {
-                        List<CustomCourse> mDataset = new ArrayList<>();
-
-                        /*
-                         * The above list is populated by a QueryDocumentSnapshot
-                         * which retrieves all documents within a collection. Each
-                         * document/object is converted into a CustomCourse class
-                         * and added.
-                         */
-                        for (QueryDocumentSnapshot q : (QuerySnapshot) object) {
-                            CustomCourse c = q.toObject(CustomCourse.class);
-                            mDataset.add(c);
-                        }
-
-                        /*
-                         * The {@link RecyclerHelper#setRecyclerView(Context, View, RecyclerView.Adapter)}
-                         * method will apply the provided data set to the given adapter which
-                         * is then presented using our recycler view. This method is and will be
-                         * used frequently, thus it has been placed within a helper class.
-                         */
-                        //noinspection ConstantConditions
-                        RecyclerView courses = findViewById(R.id.viewPagerCourses);
-                        RecyclerHelper.setRecyclerView(getApplicationContext(), courses, new CourseAdapter(mDataset, foundUid));
-                    } else
-                        Log.e("OnSuccess", object + " must be a query.");
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onFailed(FirebaseFirestoreException databaseError) {
-                Log.e("FirebaseFirestoreEx", databaseError.toString());
-            }
-        });
+        CustomAnimationUtils.alphaAnimation(linearLayoutSelector, alpha, 0, 0 / 2, true);
+        applyRecyclerView();
     }
 
-    private void setSelectedUser() {
-        //noinspection ConstantConditions
+    @OnClick(R.id.textViewSelectorTwo)
+    void textViewSelectorTwo() {
+        if (textViewSelectorTwo.getText() != null)
+            isFlashCard = false;
+
+        CustomAnimationUtils.alphaAnimation(linearLayoutSelector, alpha, 0, 0 / 2, true);
+        applyRecyclerView();
+    }
+
+    @OnClick(R.id.textViewSend)
+    void textViewSend() {
+        AnimationHelper.popAnimation(textViewSend);
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewCourses);
+        if (recyclerView != null)
+            RecyclerHelper.setRecyclerView(this, recyclerView, new CourseAdapter(null));
+
+        View view = findViewById(android.R.id.content);
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintSelector);
+        CustomAnimationUtils.circleAnimation(view, constraintLayout, duration, false);
+        CustomAnimationUtils.alphaAnimation(itemAlpha, 0, alpha, duration / 2, true);
+
+        applySelector();
+    }
+
+    private void applySelector() {
+        View view = (View) textViewSelectorOne.getParent().getParent();
+        CustomAnimationUtils.alphaAnimation(view, 0, 1, duration, true);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(onClickListenerRecycler);
+        }
+    }
+
+    private CoursesFlashCardRecyclerFragment setupViewPagerFlashCard(boolean isCreate) {
+        CoursesFlashCardRecyclerFragment fragment = new CoursesFlashCardRecyclerFragment();
+        fragment.setArguments(applyCourseBundle(isCreate));
+
+        return fragment;
+    }
+
+    private CoursesQuizRecyclerFragment setupViewPagerQuiz(boolean isCreate) {
+        CoursesQuizRecyclerFragment fragment = new CoursesQuizRecyclerFragment();
+        fragment.setArguments(applyCourseBundle(isCreate));
+
+        return fragment;
+    }
+
+    private void applyRecyclerView() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(onClickListenerHome);
+        }
+
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        if (isFlashCard) {
+            adapter.addFragment(setupViewPagerFlashCard(true));
+        } else {
+            adapter.addFragment(setupViewPagerQuiz(true));
+        }
+
+        viewPagerCourses.setPagingEnabled(false);
+        viewPagerCourses.setAdapter(adapter);
+    }
+
+    private void applySelectedUser() {
         CustomUser customUser = (CustomUser) getIntent().getSerializableExtra("user");
         foundUid = customUser.getUid();
 
-        String n = StringUtils.toUpperCase(customUser.getName());
-        String l = StringUtils.toUpperCase(customUser.getLocation());
-        TextView name = findViewById(R.id.textViewName);
-        TextView location = findViewById(R.id.textViewLocation);
-        name.setText(n);
-        location.setText(l);
+        String customUserName = StringUtils.toUpperCase(customUser.getName());
+        TextView textViewName = findViewById(R.id.textViewName);
+        textViewName.setText(customUserName);
 
-        String t = customUser.getTeacherStudent();
-        if (!StringUtils.hasMatch(t, getString(R.string.answer_teacher))) {
-            String c = customUser.getCollegeSchool();
-            if (StringUtils.hasMatch(c, getString(R.string.answer_college))) {
-                TextView course = findViewById(R.id.textViewCourse);
-                course.setText(StringUtils.toUpperCase(customUser.getCourse()));
-            }
+        TextView textViewLocationCourse = findViewById(R.id.textViewLocationCourse);
+        String status = customUser.getTeacherStudent();
+        if (StringUtils.hasMatch(status, getString(R.string.answer_teacher))) {
+            textViewLocationCourse.setText(getString(R.string.home_teacher, StringUtils.capitaliseEach(customUser.getLocation())));
+            return;
         }
+
+        String education = customUser.getCollegeSchool();
+        String location = customUser.getLocation();
+        if (!StringUtils.hasMatch(education, getString(R.string.answer_college))) {
+            textViewLocationCourse.setText(getString(R.string.home_student, StringUtils.capitaliseEach(location)));
+            return;
+        }
+
+        String l = getString(R.string.home_student, StringUtils.capitaliseEach(location));
+        String c = getString(R.string.home_course, StringUtils.capitaliseEach(customUser.getCourse()));
+        String concat = l + c;
+        textViewLocationCourse.setText(concat);
 
         int imageID = customUser.getImageID();
         if (imageID != 0) {
             ImageView image = findViewById(R.id.imageViewProfile);
             image.setImageResource(GridImageAdapterHelper.getDrawable(imageID));
         }
-
-        setCourses();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_found_user);
-        setSupportActionBar(findViewById(R.id.toolbar));
 
         ButterKnife.bind(this, findViewById(android.R.id.content));
 
-        setSelectedUser();
+        alpha = (float) getResources().getInteger(R.integer.alpha_transparent_default) / 100;
+        duration = (long) getResources().getInteger(R.integer.duration_default);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            onClickListenerHome = v -> NavigationUtils.startActivity(FoundUserActivity.this, new Intent(FoundUserActivity.this, HomeActivity.class));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(onClickListenerHome);
+        }
+
+        onClickListenerRecycler = v -> {
+            View view = findViewById(android.R.id.content);
+            ConstraintLayout constraintLayout = findViewById(R.id.constraintSelector);
+            CustomAnimationUtils.circleAnimation(view, constraintLayout, duration, false);
+            CustomAnimationUtils.alphaAnimation(constraintLayout, 1, 0, duration / 2, true);
+        };
+
+        applySelectedUser();
     }
 
     @Override
